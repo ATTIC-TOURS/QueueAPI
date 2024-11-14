15678ef2ec9c40b4e58a86bdc278603d1aa906d8
KrimssmirK <krimssmirk003@gmail.com>
2024-11-14 17:57:36 +0800

    add initial data

--- QueueAPI/queues/seed.py
+++ QueueAPI/queues/seed.py
@@ -10,12 +10,56 @@ branches = [
     {"name": "SM Davao", "password": "1234"},
 ]
 
+categories = [
+    {"name": "Japan Visa", "display_name": "Apply for Japan Visa"},
+    {"name": "Claim Passport", "display_name": "Claim Passport"},
+    {"name": "Visa Consultation", "display_name": "Visa Consultation"},
+    {"name": "Add ons", "display_name": "Add ons"},
+    {"name": "Others", "display_name": "Others"}
+]
+
+JAPAN_VISA_ID = 1
+CLAIM_PASSPORT_ID = 2
+VISA_CONSULTATION_ID = 3
+ADD_ONS_ID = 4
+OTHERS_ID = 5
+
+service_types = [
+    {"name": "Temporary Visitor", "category_id": JAPAN_VISA_ID},
+    {"name": "Multiple-Entry", "category_id": JAPAN_VISA_ID},
+    {"name": "Long Term Stay", "category_id": JAPAN_VISA_ID}
+]
+
+TEMPORARY_VISITOR_ID = 1
+MULTIPLE_ENTRY_ID = 2
+LONG_TERM_STAY_ID = 3
+
 services = [
-    {"name": "Japan Visa"},
-    {"name": "Korea Visa"},
-    {"name": "Ticket"},
-    {"name": "Claim Passport"},
-    {"name": "Others"}
+    {"name": "Tourism", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Package Tour", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Business, Conference, Cultural Exchange", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Visiting Relatives", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Visiting Friends", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Visiting US Military Personnel", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Spouse/Child of a Japanese Natioanl in the Philippines", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Transit", "category_id": JAPAN_VISA_ID, "service_type_id": TEMPORARY_VISITOR_ID},
+    {"name": "Frequent Traveler", "category_id": JAPAN_VISA_ID, "service_type_id": MULTIPLE_ENTRY_ID},
+    {"name": "Philippine Nationals with Considerable Financial Capacity", "category_id": JAPAN_VISA_ID, "service_type_id": MULTIPLE_ENTRY_ID},
+    {"name": "Business Purpose, and Cultural or Intellectual Figures", "category_id": JAPAN_VISA_ID, "service_type_id": MULTIPLE_ENTRY_ID},
+    {"name": "Student, Worker and Dependent", "category_id": JAPAN_VISA_ID, "service_type_id": LONG_TERM_STAY_ID},
+    {"name": "Diplomat/Official", "category_id": JAPAN_VISA_ID, "service_type_id": LONG_TERM_STAY_ID},
+    {"name": "Housekeeper hired by Diplomat/Official", "category_id": JAPAN_VISA_ID, "service_type_id": LONG_TERM_STAY_ID},
+    {"name": "Medical Stay", "category_id": JAPAN_VISA_ID, "service_type_id": LONG_TERM_STAY_ID},
+    {"name": "Nikkei-Jin", "category_id": JAPAN_VISA_ID, "service_type_id": LONG_TERM_STAY_ID},
+    {"name": "Filipino Parent travelling to Japan with JFC", "category_id": JAPAN_VISA_ID, "service_type_id": LONG_TERM_STAY_ID},
+    {"name": "Claim Passport", "category_id": CLAIM_PASSPORT_ID, "service_type_id": None},
+    {"name": "Visa Consultation", "category_id": VISA_CONSULTATION_ID, "service_type_id": None},
+    {"name": "Take Photograph", "category_id": ADD_ONS_ID, "service_type_id": None},
+    {"name": "Photocopy Documents", "category_id": ADD_ONS_ID, "service_type_id": None},
+    {"name": "Print Documents", "category_id": ADD_ONS_ID, "service_type_id": None},
+    {"name": "Receive Passport via LBC", "category_id": ADD_ONS_ID, "service_type_id": None},
+    {"name": "Drop-off Application", "category_id": OTHERS_ID, "service_type_id": None},
+    {"name": "Submit Add Docs", "category_id": OTHERS_ID, "service_type_id": None}
 ]
 
 windows = [
@@ -72,10 +116,26 @@ def create_default_data(apps, schema_editor):
             password=branch["password"]
         )
     
+    Category = apps.get_model("queues", "Category")
+    for category in categories:
+        Category.objects.create(
+            name=category["name"],
+            display_name=category["display_name"],
+        )
+    
+    ServiceType = apps.get_model("queues", "ServiceType")
+    for service_type in service_types:
+        ServiceType.objects.create(
+            name=service_type["name"],
+            category_id_id=service_type["category_id"],
+        )
+    
     Service = apps.get_model("queues", "Service")
     for service in services:
         Service.objects.create(
             name=service["name"],
+            category_id_id=service["category_id"],
+            service_type_id_id=service["service_type_id"]
         )
     
     Window = apps.get_model("queues", "Window")
