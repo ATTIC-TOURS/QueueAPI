6a90f9e64e05ac5ddd10acdf8591c9947876cb63
KrimssmirK <krimssmirk003@gmail.com>
2024-11-14 17:58:10 +0800

    add api

---
+++ QueueAPI/queues/views/category_api.py
@@ -0,0 +1,15 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from queues.models import Category
+from queues.serializers import CategorySerializer
+
+
+GET = "GET"
+
+@api_view([GET])
+def category_list(request, format=None):
+    if request.method == GET:
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
---
+++ QueueAPI/queues/views/service_type_api.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from queues.models import ServiceType
+from queues.serializers import ServiceTypeSerializer
+
+
+GET = "GET"
+
+@api_view([GET])
+def service_type_list(request, format=None):
+    if request.method == GET:
+        service_types = ServiceType.objects.all()
+        serializer = ServiceTypeSerializer(service_types, many=True)
+        return Response(serializer.data)
+    
+
+@api_view([GET])
+def service_type_detail_by_category(request, category_id, format=None):
+    try:
+        service_types = ServiceType.objects.filter(category_id_id=category_id)
+    except ServiceType.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == GET:
+        serializer = ServiceTypeSerializer(service_types, many=True)
+        return Response(serializer.data)
