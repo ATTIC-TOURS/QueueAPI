0c743d8a6fea2c6f753a42821d29b6e33c7fb650
KrimssmirK <krimssmirk003@gmail.com>
2024-11-19 18:11:29 +0800

    add category detail

--- QueueAPI/queues/views/category_api.py
+++ QueueAPI/queues/views/category_api.py
@@ -13,3 +13,12 @@ def category_list(request, format=None):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+    
+@api_view([GET])
+def category_detail(request, category_id, format=None):
+    if request.method == GET:
+        category = Category.objects.get(pk=category_id)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+# http GET  http://192.168.1.12:8000/categories/1/
