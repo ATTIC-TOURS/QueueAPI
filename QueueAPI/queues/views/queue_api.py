5c599266790af9d5a8dd70fa11171172fa9c5728
KrimssmirK <krimssmirk003@gmail.com>
2024-11-21 18:11:30 +0800

    add mobile queue status

--- QueueAPI/queues/views/queue_api.py
+++ QueueAPI/queues/views/queue_api.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from queues.models import Queue, Status, Service, Window, Category, Service
+from queues.models import Queue, Status, Service, Window, Category, Service, Branch
 from queues.serializers import QueueSerializer
 from django.utils import timezone
 
@@ -166,6 +166,26 @@ def generate_new_queue(branch_id, service_id, queue_no, name, email):
 
 # http POST http://192.168.1.12:8000/queues/1/1/ queue_no=1 name=kenji email=krimssmirk003@gmail.com
 
+def mobile_queue_status(data):
+    branch_name = Branch.objects.get(id=data["branch_id"]).name
+    category_name = Category.objects.get(id=data["category_id"]).name
+    service_name = Service.objects.get(id=data["service_id"]).name
+    data_for_mobile = {
+        "branch_name": branch_name,
+        "category_name": category_name,
+        "service_name": service_name,
+        "queue_no": data["queue_no"],
+        "status_id": data["status_id"],
+        "is_called": data["is_called"],
+        "code": data["code"],
+        "name": data["name"]
+    }
+    if "email" not in data:
+        return data_for_mobile 
+    data_for_mobile["email"] = data["email"]
+    return data_for_mobile
+        
+
 @api_view(["POST"])
 def queue(request, branch_id, service_id, format=None):
     # request.data -> branch_id, service_id, queue_no
@@ -223,7 +243,10 @@ def queue(request, branch_id, service_id, format=None):
             # END - notify web sockets
             
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(
+                    mobile_queue_status(serializer.data), 
+                    status=status.HTTP_201_CREATED
+                )
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
