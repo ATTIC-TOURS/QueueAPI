9c96cde7fef56a548f26f2dfbfed245cc2799425
KrimssmirK <krimssmirk003@gmail.com>
2025-02-13 14:56:22 +0800

    refactor window

--- QueueAPI/queues/serializers/WindowSerializer.py
+++ QueueAPI/queues/serializers/WindowSerializer.py
@@ -2,14 +2,7 @@ from rest_framework import serializers
 from queues.models import Window
 
 
-class WindowSerializer(serializers.Serializer):    
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(read_only=True)
-    
-    def create(self, validated_data):
-        return Window.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.save()
-        return instance
+class WindowSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Window
+        fields = "__all__"
