9e1f1d3dcd20310b67db86711d8ba5068dd7d77a
KrimssmirK <krimssmirk003@gmail.com>
2025-05-15 00:56:11 +0800

    remove window

--- QueueAPI/queues/admin/WindowAdmin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from queues.models import Window
-
-
-class WindowAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-
-admin.site.register(Window, WindowAdmin)
--- QueueAPI/queues/serializers/WindowSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from queues.models import Window
-
-
-class WindowSerializer(serializers.ModelSerializer):    
-    class Meta:
-        model = Window
-        fields = "__all__"
